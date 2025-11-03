c8fa7e7744195258894e3a404c23a614711b4bd3
MackyMa <malaika.m99@gmail.com>
2025-11-03 14:03:27 -0500

    Added users routes. ejs stuff

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require ('express');
 
 const app = express(); //caling this function ssets up a function
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res)=>{
     console.log('here');
-    res.send("Heelo World");
+    res.render("index", 
+        {user:"Macky!"});
 }); //this function will run when someone goes to the root folder
+
+app.get('/users', (req, res)=>{
+    res.send('User List');
+});
+app.get('/users/new', (req, res)=>{
+    res.send('New User Form');
+});
+
 
 app.listen(3030); //means port 3030
 
